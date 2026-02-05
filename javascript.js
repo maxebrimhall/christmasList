517e992970b526aff98d297f468f7f891ef8694e
maxebrimhall <maxe.brimhall@gmail.com>
2026-02-04 20:37:54 -0700

    Some extra for the project today.

---
+++ javascript.js
@@ -0,0 +1,12 @@
+document.querySelector("#want").addEventListener('click', add)
+
+class Item {
+    constructor(x,y){
+        this.name = x
+        this.link = y
+    }
+    add(){
+        
+    }
+
+}
