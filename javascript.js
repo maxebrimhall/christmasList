0523147b0fcbdf9d3e05fde191ee372c3d947088
maxebrimhall <maxe.brimhall@gmail.com>
2026-04-25 23:46:26 -0600

    Update input function for user and item input validation
    Fix input function to correctly query user input and item link.

--- javascript.js
+++ javascript.js
@@ -1,6 +1,14 @@
-
 document.querySelector("#want").addEventListener('click', add)
 
 function input(){
-    let names = document.querySelector("")
+    let names = document.querySelector("user-input").value
+    let itemLink = document.querySelector("item-input").value
+    let itemArr = []
+    if(names === "" || names === null){
+        return "Error, Please add a name"
+    }else if(itemLink === "" || names === null){
+        return "Error, Please add a Link"
+    }else{
+        
+    }
 }
